e1e243c95d38eb7f7d970aff63ba07fbaa11cee2
KLAKALU <player4797462226@gmail.com>
2023-04-29 22:18:43 +0900

    fix: some fix

--- Python/webapi.py
+++ Python/webapi.py
@@ -1,16 +1,12 @@
 import requests
-import json, os, time
+import json, os
 from dotenv import load_dotenv
-
-start = time.perf_counter()
 
 load_dotenv() 
 
 isbn = "9784309226712"
 
 apikey = os.getenv('WEBAPI_KEY')
-
-# pyaload = {"key1":"value1", "key2":"value2"}
 
 def getbookdetails(isbn):
     baserequest = 'https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404'
@@ -19,6 +15,7 @@ def getbookdetails(isbn):
     uri = baserequest + appid + value
     r = requests.get(uri)
     json_load = r.json()
+    print(json.dumps(json_load, indent=2, ensure_ascii=False))
     info = {}
     info["title"] = json_load['Items'][0]['Item']['title']
     info['writer'] = json_load['Items'][0]['Item']['author']
@@ -28,6 +25,6 @@ def getbookdetails(isbn):
 
 info = getbookdetails(isbn)
 
-print(info['title'])
+print("title:" + info['title'])
 
-print(info['writer'])
+print('writer' + info['writer'])
